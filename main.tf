b741c0b7b5fb032284407c12252671618f48732e
Joseph Markiewicz <49929038+JoeMark17@users.noreply.github.com>
2020-07-19 09:58:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,19 +13,53 @@
 # resource group. Think of it as a container to hold all your resources. 
 # You can find a complete list of Azure resources supported by Terraform here:
 # https://www.terraform.io/docs/providers/azurerm/
-resource "azurerm_resource_group" "tf_azure_guide" {
+resource "azurerm_resource_group" "example" {
   name     = "${var.resource_group}"
   location = "${var.location}"
   }
 
 resource "azurerm_storage_account" "example" {
   name                     = "terraformstoragetestjoe1"
-  resource_group_name      = azurerm_resource_group.tf_azure_guide.name
-  location                 = azurerm_resource_group.tf_azure_guide.location
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
   account_kind             = "StorageV2"
   access_tier              = "Hot"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "azure-functions-test-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_function_app" "example" {
+  name                       = "terraform-test-azure-functions"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  os_type                    = "linux"
+  version                    = "~2"
+  runtime_version            = "Python"
+}
+
+resource "azurerm_function_app_slot" "example" {
+  name                       = "test-azure-functions_slot"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  function_app_name          = azurerm_function_app.example.name
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+
 }
 # The next resource is a Virtual Network. We can dynamically place it into the
 # resource group without knowing its name ahead of time. Terraform handles all
